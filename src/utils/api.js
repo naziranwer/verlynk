0b178002f8cd4515ef738119352325db67c0d3c1
Nazir <anwernazir674@gmail.com>
2024-04-10 01:24:45 +0530

    https issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export const getWeatherData = async (city) => {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
     );
     return response.data;
   } catch (error) {
